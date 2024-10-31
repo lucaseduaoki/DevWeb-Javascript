656965d6951bf3151927ed5074d8b9b6d53fe4c6
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-10-31 15:42:17 -0300

    Create TesteJavascript.js

---
+++ Teste/TesteJavascript.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScript</title>
+</head>
+<body>
+    
+    <p id="paragrafo">???</p>
+
+    <h1 class= "caixa">Lucas</h1>
+    <h1 class= "caixa">Eduardo</h1>
+    <h1 class= "caixa">Aoki</h1>
+
+    <p>Real Madrid</p>
+    <p>Liverpool</p>
+    <p>Bayern de Munique</p>
+    <p>Milan</p>
+    <p>Benfica</p>
+
+    <ul>
+        <li>Eminem</li>
+        <li>Jay-Z</li>
+        <li>Tupac</li>
+        <li>Drake</li>
+        <li>Kanye West</li>
+    </ul>
+
+    <script>
+
+        let paragrafo = document.getElementById('paragrafo')
+        paragrafo.innerText = "Olá mundo!"
+        paragrafo.style.fontSize = '50px'
+
+        let elementos = document.getElementsByClassName('caixa')
+        elementos[0].style.background = 'blue'
+        elementos[1].style.background = 'blue'
+        elementos[2].style.background = 'blue'
+
+        let pr = document.getElementsByTagName('p')
+        pr[0].innerText = "Barcelona"
+        pr[1].innerText = "Manchester United"
+        pr[2].innerText = "Borussia Dortmund"
+        pr[3].innerText = "Inter de Milão"
+        pr[4].innerText = "Porto"
+
+        let pra = document.querySelector('li')
+        pra.style.fontSize = '20px'
+        pra.style.color = 'red'
+        
+    </script>
+</body>
+</html>
