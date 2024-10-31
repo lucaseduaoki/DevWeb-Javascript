34f2641464d5f8bc805ef54513d00c3095325c88
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-10-31 15:45:38 -0300

    Create Events.js

---
+++ Teste/Events.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <button onclick="alert('O botão foi clicado')">Clique Aqui</button>
+    <button ondblclick="alert('O botão foi clicado 2x')">Clique Aqui 2 Vezes</button>
+
+    <script>
+
+        function mensagem(msg){
+            console.log(msg)
+        }
+
+    </script>
+
+
+</body>
+</html>
