566be99303256315fe7d3832b22bebae8135fc05
Lucas Aoki <161040368+lucaseduaoki@users.noreply.github.com>
2024-10-31 15:46:29 -0300

    Update Atividade9.js

--- Atividades/Atividade9.js
+++ Atividades/Atividade9.js
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <img src="waddles.webp" alt=""
+    onclick="console.log('Hey! Para de me tocar, OINCC!')"
+    onmouseenter="console.log('Hey, sai com esse mouse pra lá, OINCC!')">
+     
+</body>
+</html>
